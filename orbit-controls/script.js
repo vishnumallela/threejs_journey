39854486e8081c5a04f19e372706883ea1356697
vishnumallela <mallelavishnuchoudary@gmail.com>
2025-12-21 18:24:44 -0600

    orbit-controls

--- orbit-controls/script.js
+++ orbit-controls/script.js
@@ -1,6 +1,7 @@
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js"
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js"
 
 
 const scene = new THREE.Scene();
@@ -22,7 +23,20 @@ model_loader.load("statue.obj",(obj)=>{
     camera.lookAt(obj.position)
 })
 
+const plane_mesh = new THREE.Mesh(new THREE.PlaneGeometry(window.innerWidth/2,window.innerHeight/2),new THREE.MeshPhysicalMaterial({color:"white",roughness:0.2,metalness:0.5,reflectivity:0.5,transparent:true,opacity:0.1}))
+plane_mesh.position.set(0,-5,0)
+plane_mesh.rotation.x = -Math.PI/2
+scene.add(plane_mesh)
+
+const rgbeLoader = new RGBELoader()
+rgbeLoader.load("sky.hdr",(environmentMap)=>{
+    environmentMap.mapping = THREE.EquirectangularReflectionMapping
+    scene.background = environmentMap
+    scene.environment = environmentMap
+})
+
 const controls = new OrbitControls(camera,renderer.domElement)
+controls.enableDamping = true
 
 const animate = () => {
     requestAnimationFrame(animate)
